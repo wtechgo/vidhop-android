95630aa0ae54d2097957f5620664bc7776e733c0
wtechgo <wtechgo@proton.me>
2022-10-12 09:54:01 +0200

    Fix dl fb post bug.

--- bin/facebook-scraper.py
+++ bin/facebook-scraper.py
@@ -21,6 +21,35 @@ class FacebookScraper:
         except TypeError:
             return ""
 
+    def convert_none_type(self, value):
+        if value is None:
+            return ""
+        return value
+
+    def convert_list_of_none_types(self, a_list):
+        new_list = []
+        for item in a_list:
+            if item is None:
+                item = ""
+            if isinstance(item, list):
+                self.convert_list_of_none_types(item)
+            new_list.append(item)
+        return new_list
+
+    def convert_datetimes_to_strings_in_dict_recursive(self, d) -> dict:
+        updated_dict = {}
+        for k, v in d.items():
+            if isinstance(v, dict):
+                updated_dict[k] = self.convert_datetimes_to_strings_in_dict_recursive(v)
+            elif isinstance(v, list):
+                updated_dict[k] = self.convert_list_of_none_types(v)
+            else:
+                if isinstance(v, datetime.date):
+                    updated_dict[k] = v.strftime("%Y-%m-%d %H:%M:%S")
+                else:
+                    updated_dict[k] = self.convert_none_type(v)
+        return updated_dict
+
     def stringify_comments_full(self, post):
         comments = {
             'comment_id': self.value_for_key(post, 'comment_id'),
@@ -40,53 +69,8 @@ class FacebookScraper:
         return comments
 
     # post = next(posts)
-    def create_json_post(self, post: dict):
-        # print({post['time']})
-        # print({post['fetched_time']})
-
-        json_post = {
-            'original_request_url': self.value_for_key(post, 'original_request_url'),
-            'post_url': self.value_for_key(post, 'post_url'), 'post_id': self.value_for_key(post, 'post_id'),
-            'text': self.value_for_key(post, 'text'), 'post_text': self.value_for_key(post, 'post_text'),
-            'shared_text': self.value_for_key(post, 'shared_text'),
-            'original_text': self.value_for_key(post, 'original_text'),
-            'time': self.value_for_key(post, 'time'), 'timestamp': self.value_for_key(post, 'timestamp'),
-            'image': self.value_for_key(post, 'image'),
-            'image_lowquality': self.value_for_key(post, 'image_lowquality'),
-            'images': self.value_for_key(post, 'images'),
-            'images_description': self.value_for_key(post, 'images_description'),
-            'images_lowquality': self.value_for_key(post, 'images_lowquality'),
-            'images_lowquality_description': self.value_for_key(post, 'images_lowquality_description'),
-            'video': self.value_for_key(post, 'video'),
-            'video_duration_seconds': self.value_for_key(post, 'video_duration_seconds'),
-            'video_height': self.value_for_key(post, 'video_height'),
-            'video_id': self.value_for_key(post, 'video_id'),
-            'video_quality': self.value_for_key(post, 'video_quality'),
-            'video_size_MB': self.value_for_key(post, 'video_size_MB'),
-            'video_thumbnail': self.value_for_key(post, 'video_thumbnail'),
-            'video_watches': self.value_for_key(post, 'video_watches'),
-            'video_width': self.value_for_key(post, 'video_width'), 'likes': self.value_for_key(post, 'likes'),
-            'comments': self.value_for_key(post, 'comments'), 'shares': self.value_for_key(post, 'shares'),
-            'link': self.value_for_key(post, 'link'), 'links': self.value_for_key(post, 'links'),
-            'user_id': self.value_for_key(post, 'user_id'), 'username': self.value_for_key(post, 'username'),
-            'user_url': self.value_for_key(post, 'user_url'), 'is_live': self.value_for_key(post, 'is_live'),
-            'factcheck': self.value_for_key(post, 'factcheck'),
-            'shared_post_id': self.value_for_key(post, 'shared_post_id'),
-            'shared_time': self.value_for_key(post, 'shared_time'),
-            'shared_user_id': self.value_for_key(post, 'shared_user_id'),
-            'shared_username': self.value_for_key(post, 'shared_username'),
-            'shared_post_url': self.value_for_key(post, 'shared_post_url'),
-            'available': self.value_for_key(post, 'available'),
-            'comments_full': self.stringify_comments_full(post['comments_full']),  # important
-            'reactors': self.value_for_key(post, 'reactors'),
-            'w3_fb_url': self.value_for_key(post, 'w3_fb_url'),
-            'reactions': self.value_for_key(post, 'reactions'),
-            'reaction_count': self.value_for_key(post, 'reaction_count'),
-            'with': self.value_for_key(post, 'with'), 'page_id': self.value_for_key(post, 'page_id'),
-            'sharers': self.value_for_key(post, 'sharers'), 'image_id': self.value_for_key(post, 'image_id'),
-            'image_ids': self.value_for_key(post, 'image_ids')}
-
-        return json_post
+    def create_json_post(self, post):
+        return self.convert_datetimes_to_strings_in_dict_recursive(post)
 
     def silence_warning(self):
         # Suppresses the following 2 warnings.
@@ -104,8 +88,12 @@ class FacebookScraper:
                 options={"comments": True}
             )
         )
-        post_json = self.create_json_post(posts[0])
-        print(json.dumps(post_json))
+        post = posts[0]
+        sanitized_post = self.convert_datetimes_to_strings_in_dict_recursive(post)
+        # sanitized_post = self.create_json_post(post)
+        json_post = json.dumps(sanitized_post)
+        print(json_post)
+        return json_post
 
 
 if __name__ == "__main__":
